f587e616f441f3cb392a638ad78e8df186336f0b
Joni <joni.makela@gmail.com>
2018-09-28 12:54:06 +0300

    Packaged main functionality into a lib

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@
 #![allow(unknown_lints)]
 #![warn(clippy::all)]
 
+extern crate minipliinterpreter as minipli;
+use minipli::parser::interpreter::InterpreterState;
+
 pub mod lexer;
 pub mod parser;
 
@@ -18,11 +21,11 @@ use simplelog::LogLevelFilter;
 use simplelog::TermLogger;
 use structopt::StructOpt;
 
-use crate::lexer::lexeme_iterator::LexemeIterator;
-use crate::parser::interpreter::Interpreter;
-use crate::parser::interpreter::InterpreterState;
-use crate::parser::token::Token;
-use crate::parser::token_iterator::TokenIterator;
+//use crate::lexer::lexeme_iterator::LexemeIterator;
+//use crate::parser::interpreter::Interpreter;
+//use crate::parser::interpreter::InterpreterState;
+//use crate::parser::token::Token;
+//use crate::parser::token_iterator::TokenIterator;
 
 use std::env;
 use std::fs::File;
@@ -118,7 +121,7 @@ fn main() {
         info!("File read");
         buffer.trim_left_matches("\u{feff}");
 
-        let _ = eval_file(&buffer);
+        let _ = minipli::eval_file(&buffer);
     } else {
         println!("Type commands");
         println!("Type {:?} to exit", exit_keyword);
@@ -133,74 +136,7 @@ fn main() {
             if input_text.to_string().trim() == exit_keyword {
                 break;
             }
-            eval_line(&input_text.to_string().trim(), &mut state);
+            minipli::eval_line(&input_text.to_string().trim(), &mut state);
         }
     };
-}
-
-fn eval_file(file_contents: &str) -> Result<(), String> {
-    let lexeme_it = LexemeIterator::new(file_contents);
-    let mut token_iterator: TokenIterator<LexemeIterator<'_>> = TokenIterator::new(lexeme_it);
-    let mut state = InterpreterState::new();
-    let mut interpreter = Interpreter::new(&mut token_iterator as &mut dyn Iterator<Item = Token>, &mut state);
-    interpreter.interpret()
-}
-
-fn eval_line(line: &str, mut state: &mut InterpreterState) {
-    debug!("{:?}", line);
-    let it = LexemeIterator::new(line);
-    let mut token_iterator: TokenIterator<LexemeIterator<'_>> = TokenIterator::new(it);
-    let mut interpreter = Interpreter::new(&mut token_iterator, &mut state);
-    if let Err(msg) = interpreter.interpret() {
-        error!("{}", msg)
-    }
-}
-
-#[test]
-fn sample1_var_definition_expression_print() {
-    let code = r#"
-var X : int := 4 + (6 * 2);
-print X;
-"#;
-    eval_file(code).unwrap();
-}
-
-#[test]
-fn sample2_loop_print() {
-    let code = r#"
-var nTimes : int := 0;
-print nTimes;
-print "How many times?";
-nTimes := 3;
-print nTimes;
-var x : int;
-for x in 0..nTimes-1 do
-	print x;
-	print " : Hello, World!\n";
-end for;
-print x;
-print nTimes;
-assert (x = (nTimes-1));
-"#;
-    eval_file(code).unwrap();
-}
-
-#[test]
-fn sample4_decl_assign_print() {
-    let code = r#"
-var X : int;
-X := 15;
-print X;
-"#;
-    eval_file(code).unwrap();
-}
-
-#[test]
-fn test_decl_assign_boolean() {
-    let code = r#"
-var X : bool;
-X := 15;
-print X;
-"#;
-    eval_file(code).unwrap();
 }
